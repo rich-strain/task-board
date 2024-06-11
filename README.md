a60ae70bf4ba723273b24567c28a02017ca26e77
Rich <richardstrain@gmail.com>
2024-06-11 09:02:24 -0400

    Modal Displays On Button Click

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Task Board Starter Code
+
+## Description
+
+Create a simple task board application that allows a team to manage project tasks by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+You'll need to use the Day.js Links to an external site. library to work with dates. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+
+- GIVEN a task board to manage a project
+- WHEN I open the task board
+- THEN the list of project tasks is displayed in columns representing the task progress state (Not Yet Started, In Progress, Completed)
+- WHEN I view the task board for the project
+- THEN each task is color coded to indicate whether it is nearing the deadline (yellow) or is overdue (red)
+- WHEN I click on the button to define a new task
+- THEN I can enter the title, description and deadline date for the new task into a modal dialog
+- WHEN I click the save button for that task
+- THEN the properties for that task are saved in localStorage
+- WHEN I drag a task to a different progress column
+- THEN the task's progress state is updated accordingly and will stay in the new column after refreshing
+- WHEN I click the delete button for a task
+- THEN the task is removed from the task board and will not be added back after refreshing
+- WHEN I refresh the page
+- THEN the saved tasks persist
+
+## Deployed URL
+
+- https://rich-strain.github.io/task-board/
+
+## Screenshot
+
+![https://github.com/rich-strain/task-board](./assets/images/screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,56 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem('tasks'));
+let nextId = JSON.parse(localStorage.getItem('nextId'));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  // use crypto.randomUUID() to create a unique id
+  let taskId = crypto.randomUUID();
+  return taskId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+  //create card elements and classes
+  // append card elements to card
+  // return card?
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+  // makes cards draggable with jQuery UI
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
+function handleAddTask(event) {
+  // prevent default form submission
+  event.preventDefault();
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
+function handleDeleteTask(event) {
+  // delete task by Unique ID
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+  // update the task to the new status lane
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  // render the task list
+  //renderTaskList();
 
-});
+  // add event listeners
+  // $('#addTaskForm').on('submit', handleAddTask);
+  // $('#taskList').on('click', '.delete', handleDeleteTask);
+
+  // make lanes droppable
+  // $('.lane').droppable({
+  //   drop: handleDrop,
+  // });
+
+  // make due date a date picker
+  $('#dueDate').datepicker();
+}); // end of document ready
